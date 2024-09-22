11104da33ba9bc5403678639bb3beba0a6d89806
Barasa <timbaros87@gmail.com>
2024-09-22 14:57:11 +0300

    style pages

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/*toggle icon navbar*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
 /*scroll section active link*/
 let sections = document.querySelectorAll('section')
 let navLinks = document.querySelectorAll('header nav a')
@@ -22,4 +32,32 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
+/*remove toggle icon and navbar when click navbar link (scroll)*/
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
 };
+
+// scroll reveal
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-image, .services-container, .portfolio-box, contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/* typed js */
+const typed = new Typed('.multiple-text', {
+    strings: ['Software Engineer', 'Fullstack Developer', 'Graphic Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
