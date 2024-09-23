929e7cde57ac104b9ee7cddf161a64981e1ce2d9
Barasa <timbaros87@gmail.com>
2024-09-23 17:34:54 +0300

    update About, Services and Portfolio sections

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-/*toggle icon navbar*/
+/* Click function for About Section */
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('read-more-btn').onclick = function () {
+        var moreText = document.getElementById("more-text");
+        var btnText = document.getElementById("read-more-btn");
+
+        if (moreText.style.display === "none") {
+            moreText.style.display = "inline";
+            btnText.innerHTML = "Read Less"; 
+        } else {
+            moreText.style.display = "none";
+            btnText.innerHTML = "Read More"; 
+        }
+    };
+});
+
+/* Click function for Service Section */
+document.addEventListener("DOMContentLoaded", function() {
+    const readMoreButtons = document.querySelectorAll('.btn.read-more');
+    readMoreButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const moreText = document.getElementById(this.getAttribute('data-target'));
+            const btnText = this;
+            if (moreText.style.display === "none" || moreText.style.display === "") {
+                moreText.style.display = "inline";
+                btnText.innerHTML = "Read Less";
+            } else {
+                moreText.style.display = "none";
+                btnText.innerHTML = "Read More";
+            }
+        });
+    });
+});
+
+/* Toggle icon navbar */
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -7,12 +41,27 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
+/* Function for debounce */
+function debounce(func, wait = 15, immediate = true) {
+    let timeout;
+    return function() {
+        let context = this, args = arguments;
+        let later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
 
-/*scroll section active link*/
-let sections = document.querySelectorAll('section')
-let navLinks = document.querySelectorAll('header nav a')
+/* Scroll section active link */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
+window.onscroll = debounce(function() {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
@@ -24,28 +73,24 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-        };
+        }
     });
 
-    /*sticky navbar*/
+    /* Sticky navbar */
     let header = document.querySelector('header');
-
     header.classList.toggle('sticky', window.scrollY > 100);
 
-/*remove toggle icon and navbar when click navbar link (scroll)*/
-
+    /* Remove toggle icon and navbar when click navbar link (scroll) */
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+});
 
-};
-
-// scroll reveal
+/* Scroll reveal */
 ScrollReveal({
     reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200
-
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
@@ -53,11 +98,11 @@ ScrollReveal().reveal('.home-image, .services-container, .portfolio-box, contact
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
-/* typed js */
+/* Typed.js */
 const typed = new Typed('.multiple-text', {
     strings: ['Software Engineer', 'Fullstack Developer', 'Graphic Designer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
-})
+});
